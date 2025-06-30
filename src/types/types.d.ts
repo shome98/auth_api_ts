4ffa3a47354c500cf916565e2b97b7937d0be241
Sowparna Shome <soupornoshome@gmail.com>
2025-07-01 01:33:21 +0530

    types set

---
+++ src/types/types.d.ts
@@ -0,0 +1,15 @@
+import { Connection } from "mongoose";
+
+declare global {
+  var mongoose: {
+    conn: Connection | null;
+    promise: Promise<Connection> | null;
+  };
+  namespace Express {
+    interface Request {
+      userId?: string;
+      accessToken?: string;
+    }
+  }
+}
+export {};
