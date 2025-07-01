8fb2a5a30b3b9264ade1594619a040063574187e
Sowparna Shome <soupornoshome@gmail.com>
2025-07-01 23:24:05 +0530

    create

--- src/utils/user.services.ts
+++ src/utils/user.services.ts
@@ -1,21 +1,32 @@
-import { IUser, User } from "../models/user.model"
+import { IUser, User } from "../models/user.model";
 
-const findExistingUSerByEmail = async (email: string) => {
-    try {
-        const existingUser = await User.findOne({ email });
-        return existingUser;
-    } catch (error) {
-        console.error("❌ could not find the user.", error);
-        throw error;
-    }
-}
+export const findExistingUserByEmail = async (email: string) => {
+  try {
+    const existingUser = await User.findOne({ email });
+    return existingUser;
+  } catch (error) {
+    console.error("❌ could not find the user.", error);
+    throw error;
+  }
+};
 
-const createUser = async (obj: IUser) => {
-    try {
-        const newUser = await User.create({ ...obj });
-        return newUser;
-    } catch (error) {
-        console.error("❌ Could not create the user.");
-        throw error;
-    }
-}
+export const createUser = async (obj: IUser) => {
+  try {
+    const newUser = await User.create({ ...obj });
+    const createdUser = await User.findById(newUser._id);
+    return createdUser;
+  } catch (error) {
+    console.error("❌ Could not create the user.");
+    throw error;
+  }
+};
+
+export const findExistingUserWithPassword = async (email?: string,username?:string) => {
+  try {
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] }).select("+password");
+    return existingUser;
+  } catch (error) {
+    console.error("❌ could not find the user.", error);
+    throw error;
+  }
+};
