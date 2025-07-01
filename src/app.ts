67a80835ca4e6839ef9174d19205bd8e9a071a20
Sowparna Shome <soupornoshome@gmail.com>
2025-07-01 18:42:15 +0530

    use routes and others

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes";
+import connectToDatabase from "./db/connect.db";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Your express application configuration
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("😊 Welcome to api!!!!");
 });
 
-// Listen on the specified port; note that Vercel provides a port automatically
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
-});
+app.use("/api/v1/auth", authRoutes);
+
+connectToDatabase()
+  .then(() => console.log("✅ Connected to database"))
+  .catch((error) => console.log("❌ Database connection Failed with ", error));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running at http://localhost:${process.env.PORT}`)
+);
 
 export default app;
