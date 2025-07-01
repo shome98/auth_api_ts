a4f297386ab0672bdcb8e2799d25658bb7e9c846
Sowparna Shome <soupornoshome@gmail.com>
2025-07-01 18:41:39 +0530

    utility add

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+export const asyncHandler = (
+  requestHandler: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<any>
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
