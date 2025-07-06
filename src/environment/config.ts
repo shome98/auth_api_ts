20c88cc05ccf388357f62c2b8a9d8776b3e31c62
Sowparna Shome <soupornoshome@gmail.com>
2025-07-06 23:26:21 +0530

    fix refresh token

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -5,7 +5,7 @@ const config = {
   JWT_SECRET: String(process.env.JWT_SECRET),
   JWT_EXPIRY: Number(process.env.JWT_EXPIRY),
   JWT_REFRESH_SECRET: String(process.env.JWT_REFRESH_SECRET),
-  JWT_REFRESH_EXPIRY: Number(process.env.JWT_REFREH_EXPIRY),
+  JWT_REFRESH_EXPIRY: Number(process.env.JWT_REFRESH_EXPIRY),
   PORT: Number(process.env.PORT),
 };
 export default config;
