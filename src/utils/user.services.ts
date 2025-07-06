1713225a8f1e007d1d9bb62554c61d163793a2ff
Sowparna Shome <soupornoshome@gmail.com>
2025-07-06 23:32:25 +0530

    token update or delete

--- src/utils/user.services.ts
+++ src/utils/user.services.ts
@@ -1,3 +1,4 @@
+import RefreshTokenModel, { IRefreshToken } from "../models/refreshToken.model";
 import { IUser, User } from "../models/user.model";
 
 export const findExistingUserByEmail = async (email: string) => {
@@ -13,20 +14,53 @@ export const findExistingUserByEmail = async (email: string) => {
 export const createUser = async (obj: IUser) => {
   try {
     const newUser = await User.create({ ...obj });
-    const createdUser = await User.findById(newUser._id);
-    return createdUser;
+    return newUser;
   } catch (error) {
-    console.error("❌ Could not create the user.");
+    console.error("❌ Could not create the user.", error);
     throw error;
   }
 };
 
-export const findExistingUserWithPassword = async (email?: string,username?:string) => {
+export const findExistingUserWithPassword = async (
+  email?: string,
+  username?: string
+) => {
   try {
-    const existingUser = await User.findOne({ $or: [{ email }, { username }] }).select("+password");
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }],
+    }).select("+password");
     return existingUser;
   } catch (error) {
     console.error("❌ could not find the user.", error);
+    throw error;
+  }
+};
+
+export const addOrUpdateRefreshToken = async (tokenObj: {
+  userId: string;
+  deviceInfo: string;
+  token: string;
+  expiresAt: Date;
+}) => {
+  try {
+    const { userId, ...updateData } = tokenObj;
+
+    const newOrUpdatedDocument = await RefreshTokenModel.findOneAndUpdate(
+      { userId },
+      { $set: updateData },
+      {
+        new: true,
+        upsert: true,
+        runValidators: true,
+      }
+    );
+
+    return newOrUpdatedDocument;
+  } catch (error) {
+    console.error(
+      "❌ Could not create/update the refresh token document.",
+      error
+    );
     throw error;
   }
 };
