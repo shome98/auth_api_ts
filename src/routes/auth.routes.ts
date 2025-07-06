b81e18248dbc62c0d73732a9bf770f3c3e0466cc
Sowparna Shome <soupornoshome@gmail.com>
2025-07-06 23:26:32 +0530

    login route add

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
-import { register } from "../controllers/auth.controller";
+import { login, register } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", asyncHandler(register));
+router.post("/login", asyncHandler(login));
 
 export default router;
